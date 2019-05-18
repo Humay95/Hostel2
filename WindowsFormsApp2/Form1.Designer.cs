6313e77f46a5bc93b13ec6087e372be4c90b2071
Humay <humaysadiqovaaa95@gmail.com>
2019-05-18 06:43:32 -0700

    Hostel

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnadmin = new System.Windows.Forms.Button();
+            this.btnworkers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Purple;
+            this.label1.Location = new System.Drawing.Point(222, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 60);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Reservation";
+            // 
+            // btnadmin
+            // 
+            this.btnadmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadmin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnadmin.Location = new System.Drawing.Point(35, 274);
+            this.btnadmin.Name = "btnadmin";
+            this.btnadmin.Size = new System.Drawing.Size(136, 39);
+            this.btnadmin.TabIndex = 3;
+            this.btnadmin.Text = "Admin";
+            this.btnadmin.UseVisualStyleBackColor = true;
+            this.btnadmin.Click += new System.EventHandler(this.Btnadmin_Click);
+            // 
+            // btnworkers
+            // 
+            this.btnworkers.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnworkers.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnworkers.Location = new System.Drawing.Point(617, 274);
+            this.btnworkers.Name = "btnworkers";
+            this.btnworkers.Size = new System.Drawing.Size(136, 39);
+            this.btnworkers.TabIndex = 4;
+            this.btnworkers.Text = "Workers";
+            this.btnworkers.UseVisualStyleBackColor = true;
+            this.btnworkers.Click += new System.EventHandler(this.Btnworkers_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnworkers);
+            this.Controls.Add(this.btnadmin);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Hostel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnadmin;
+        private System.Windows.Forms.Button btnworkers;
+    }
+}
+
